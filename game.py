12f8044e3d9ad4e7356b169391f61df429a6d14e
berna39 <kalemajoseph39@gmail.com>
2022-01-13 22:53:49 +0200

    Letters entrered by the user

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 from words import words_dictionary
 
@@ -7,3 +8,11 @@ def get_valid_word(words):
     word = random.choice(words)
     while '_' in word or ' ' in word:
         word = random.choice(words)
+    return word
+
+
+def hangman():
+    valid_word = get_valid_word(words_dictionary) #retrieveing a valid word
+    word_letters = set(valid_word) #get the word as a set
+    alphabet = set(string.ascii_uppercase) #all ascii letter in eglish
+    user_letters = set()  #letters already entered by the user
