eeb6323104c35996d67cc736fefd8de6bd35e6dc
berna39 <kalemajoseph39@gmail.com>
2022-01-13 23:03:15 +0200

    Cchecking user's entries

--- game.py
+++ game.py
@@ -15,4 +15,19 @@ def hangman():
     valid_word = get_valid_word(words_dictionary) #retrieveing a valid word
     word_letters = set(valid_word) #get the word as a set
     alphabet = set(string.ascii_uppercase) #all ascii letter in eglish
-    user_letters = set()  #letters already entered by the user
+    used_letters = set()  #letters already entered by the user
+
+    #getting user's input
+    user_letter = input('Guess a letter').upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+        elif user_letter in used_letters:
+            print("you've alreadu used that character")
+        else:
+            print("worng character")
+
+
+
+
